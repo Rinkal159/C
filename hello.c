2dfd12f7c2bfde1d7eb5ebf6979f9dca3cb6fbee
rinkal159 <rinkalsingapuri@gmail.com>
2026-01-12 21:45:11 +0530

    Introduces data types in c and how to print different values correctly.

--- hello.c
+++ hello.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
-int main(void) {
+int main()
+{
     printf("Hello world!\n");
+
+    // int
+    int a = 4;
+    printf("int is %d\n", a);
+
+    // float
+    float floatNum = 3.14;
+    printf("float num is %f\n", floatNum);
+
+    // double
+    double doubleNum = 5.16789;
+    printf("double jum is %lf\n", doubleNum);
+
+    // char
+    char chr = 'a';
+    printf("char is %c\n", chr);
+
+    // string
+    char string[] = "Rinkal";
+    printf("string is %s\n", string);
 }
