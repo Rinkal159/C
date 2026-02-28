afdf8ff3320bb26d61e22a5e3ccaadae12039c8e
rinkal159 <rinkalsingapuri@gmail.com>
2026-02-28 14:49:56 +0530

    Practices pointer and adress.

--- practice.c
+++ practice.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
 
-void printPattern(int n)
-{
-    if (n == 0)
-    {
-        return;
-    }
-
-    printPattern(n - 1);
-
-    for (int i = 1; i <= n; i++)
-    {
-        printf("*");
-    }
-
-    printf("\n");
-};
-
-int main(void)
-{
-    int num;
-    printf("n: ");
-    scanf("%d", &num);
-
-    printPattern(num);
+int main(void) {
+    int n = 4;
+    printf("%p\n", &n);
 }
